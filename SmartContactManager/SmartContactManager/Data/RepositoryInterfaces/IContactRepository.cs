5dee90e067858446adf2146a651c71ab5e2d4672
jwalit21 <jwalitshah2q@gmail.com>
2021-03-18 00:11:42 +0530

    Done : Contact API

---
+++ SmartContactManager/SmartContactManager/Data/RepositoryInterfaces/IContactRepository.cs
@@ -0,0 +1,20 @@
+﻿using SmartContactManager.Models;
+using SmartContactManager.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartContactManager.Data.RepositoryInterfaces
+{
+    public interface IContactRepository
+    {
+        IEnumerable<Contact> GetAllContacts();
+/*        IEnumerable<Contact> GetAllContactsByUserId(int id);*/
+        Contact GetContactById(int id);
+        Contact UpdateContact(Contact contact);
+        Contact CreateContact(CreateContact contact);
+        Contact DeleteContact(Contact contact);
+        bool DoesBelongsToUser(Contact contact, int userId);
+    }
+}
